77778d7b8c6a25063c9772c17ead6e4b555ed372
tphat <138270919+huynhletanphat@users.noreply.github.com>
2024-02-04 16:27:44 +0700

    Delete fblogin/fblogin/src/main directory

--- fblogin/fblogin/src/main/helpers/user_operations.py
+++
@@ -1,5 +0,0 @@
-import json
-
-def load_users(users_file):
-    with open(users_file, "r") as f:
-        return json.load(f)
--- fblogin/fblogin/src/main/helpers/webdriver_operations.py
+++
@@ -1,8 +0,0 @@
-import time
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-
-def create_driver():
-    chrome_options = Options()
-    chrome_options.add_argument("--disable-notifications")
-    return webdriver.Chrome(options=chrome_options)
